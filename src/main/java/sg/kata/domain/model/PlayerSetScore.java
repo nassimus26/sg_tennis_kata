2aa81b92951daaeb88ea36eb8e01ae777c4d8ea8
Nassim MOUALEK <nassim.moualek-ext@jcdecaux.com>
2018-09-03 15:04:48 +0200

    Implements SPRINT_2 User Sotry 1 First To Seven Points

---
+++ src/main/java/sg/kata/domain/model/PlayerSetScore.java
@@ -0,0 +1,10 @@
+package sg.kata.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Nassim MOUALEK on 31/08/2018.
+ */
+public class PlayerSetScore extends PlayerScore {
+}
---
+++ src/main/java/sg/kata/domain/score/SetScoreExecutor.java
@@ -0,0 +1,35 @@
+package sg.kata.domain.score;
+import lombok.extern.slf4j.Slf4j;
+import sg.kata.domain.model.IGameEngine;
+
+@Slf4j
+public class SetScoreExecutor extends ScoreExecutor {
+
+    public SetScoreExecutor(IGameEngine game){
+        super(game);
+    }
+
+    @Override
+    public void applyRules(String player){
+
+        String opponent = game.getOpponentName(player);
+        int currentScore = game.getPlayerSetScore(player);
+        int newScore = nextScore(currentScore);
+        if ( newScore >= 6 ) {
+            if (game.getPlayerSetScore(opponent) <=4 || newScore==7 ) {
+                game.winTheSet(player);
+                game.setPlayerSetScore( player, newScore );
+            }else
+                game.setPlayerSetScore( player, newScore );
+        }else
+            game.setPlayerSetScore( player, newScore );
+        log.debug( "New Set Score " + game.serialize() );
+
+    }
+
+    @Override
+    protected int nextScore(int score){
+        return score+1;
+    }
+
+}
